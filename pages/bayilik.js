93546926c48dc6031f298088fb44a4858eb3fe98
berke-cura <berkecura@protonmail.com>
2024-07-26 19:50:22 +0300

    fix:okul

--- pages/bayilik.js
+++ pages/bayilik.js
@@ -1,9 +1,9 @@
 // Home.js
 import Head from "next/head";
-import { Open_Sans } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { Roboto } from "next/font/google";
+import styles from "@/styles/Bayi.module.css";
 
-const openSans = Open_Sans({ subsets: ["latin"], weight: ["400", "700"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Home() {
   return (
@@ -14,11 +14,53 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main className={`${styles.main} ${openSans.className}`}>
-        <div id="section1" className={styles.section} style={{ backgroundColor: '#ffcccc' }}>
-          <p>BAYİLİK</p>
+      <main className={`${styles.main} ${roboto.className}`}>
+        <div id="section1" className={styles.section1} >
+          <h1>Gücüne güç kattığımız bayilerimiz arasına katılın!</h1>
+          <p>Hızla büyüyen e-öğrenme ekosisteminde aktif rol almak isteyenleri Mathsey Matematik Robotu Bayisi olmaya davet ediyoruz.</p>
         </div>
-
+        <div id="section2" className={styles.section2} >
+          <h2>Neden Mathsey Bayisi Olmalıyım?</h2>
+          <div className={styles.features}>
+            <div className={styles.feature}>
+              <img src="../gelir.png" alt="Yüksek Gelir" className={styles.icon} />
+              <p>Yüksek Gelir</p>
+            </div>
+            <div className={styles.feature}>
+              <img src="../stoksuz.png" alt="Stoksuz Satış" className={styles.icon} />
+              <p>Stoksuz Satış</p>
+            </div>
+            <div className={styles.feature}>
+              <img src="../sürekli.png" alt="Sürekli Büyüyen E-Öğrenme Ekosistemi" className={styles.icon} />
+              <p>Sürekli Büyüyen E-Öğrenme Ekosistemi</p>
+            </div>
+            <div className={styles.feature}>
+              <img src="../sistem.png" alt="Sistemiyle Türkiye’de İlk ve Tek" className={styles.icon} />
+              <p>Sistemiyle Türkiye’de İlk ve Tek</p>
+            </div>
+          </div>
+        </div>
+        <div id="section3" className={styles.section3} >
+        <h2>Mathsey Matematik Robotu nedir?</h2>
+        <p2>Türkiye’nin 77 ilinde ve dünyada 8 ülkede, öğrenci ve velilerine %100’e yakın memnuniyetle hizmet veren Matematik Röntgeni’nin geliştirdiği, öğrencinin matematik temelindeki eksikleri “Öğrenciye Özel Dijital Analiz ve Programla” hızla ve eksiksiz kapatan bir video eğitim paketidir.</p2>
+          <div className={styles.features}>
+            <div className={styles.feature}>
+              <img src="../dijital.png" alt="Yüksek Gelir" className={styles.icon} />
+              <p>Dijital Analizle Eksikleri Nokta Atışı Tespit</p>
+            </div>
+            <div className={styles.feature}>
+              <img src="../hizla.png" alt="Stoksuz Satış" className={styles.icon} />
+              <p>Yapay Zeka ile Özelleşen Dersler</p>
+            </div>
+            <div className={styles.feature}>
+              <img src="../mentorTa.png" alt="Sürekli Büyüyen E-Öğrenme Ekosistemi" className={styles.icon} />
+              <p>Mentor Takibi</p>
+            </div>
+          </div>
+        </div>
+        <div id="section4" className={styles.section4} >
+          <p>BAYİLİK-4</p>
+        </div>
       </main>
     </>
   );
--- pages/okullara-ozel.js
+++ pages/okullara-ozel.js
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import { Open_Sans } from "next/font/google";
 import styles from "@/styles/Okul.module.css";
+import Image from "next/image";
 
 const openSans = Open_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MATHSEY | Matematik Robotu</title>
+        <title>MATHSEY | Kurumsal</title>
         <meta name="description" content="Empowering K-12 students with high-quality online math course videos" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
@@ -17,22 +18,22 @@ export default function Home() {
       <main className={`${styles.main} ${openSans.className}`}>
         <div id="section1" className={styles.section} style={{ backgroundColor: '#ffccfc' }}>
           <div className={styles.content}>
-            <h1>Kurumsal | MATHSEY</h1>
-            <p>Öğrencilerin matematik başarısını 2,5 kat* artıran Mathsey Matematik Robotu, kurumsal çözümleriyle şimdi okullarda!</p>
-            <p>8-9-10-11 VE 12. SINIF ÖĞRENCİLERİ İÇİN!</p>
-            <p>Okullar ve Eğitim Kurumları için başarıya götüren çok avantajlı çözümler!</p>
-            <p>Mathsey Temel Matematik Çözüm Programı + Her 100 öğrenciniz için bir öğretmeninize özel öğretmen üyeliği</p>
+          <div className={styles.logoDiv}>
+            <Image className={styles.logo} src="/logo.png" alt="Mathsey Logo" width={200} height={100} />
+          </div>
+          <p>Öğrencilerin matematik başarısını <b>2,5 kat* artıran Mathsey Matematik Robotu,</b> kurumsal çözümleriyle şimdi okullarda!</p>
+          <p2>8-9-10-11 VE 12. SINIF ÖĞRENCİLERİ İÇİN!</p2>
           </div>
         </div>
         <div id="section2" className={styles.section2} style={{ backgroundColor: '#fbd8be' }}>
           <div className={styles.content}>
-            <h1>Okullar ve Eğitim Kurumları için başarıya götüren çok avantajlı çözümler!</h1>
+            <h1><b>Okullar ve Eğitim Kurumları için başarıya götüren çok avantajlı çözümler!</b></h1>
             <p>Mathsey Temel Matematik Çözüm Programı + Her 100 öğrenciniz için bir öğretmeninize Mentor Eğitimi (Eğitim ücretsiz verilecektir.)</p>
             <h2>Ücretlendirme</h2>
-            <p>1-10 Öğrenci: 40.000 ₺</p>
-            <p>11-100 Öğrenci: 120.000 ₺</p>
-            <p>101-500 Öğrenci: 200.000 ₺</p>
-            <p>501-1000 Öğrenci: 320.000 ₺</p>
+            <p>1-10 Öğrenci: <b>40.000 ₺</b></p>
+            <p>11-100 Öğrenci: <b>120.000 ₺</b></p>
+            <p>101-500 Öğrenci: <b>200.000 ₺</b></p>
+            <p>501-1000 Öğrenci: <b>320.000 ₺</b></p>
             <p>Belirtilen fiyatlara KDV dahil değildir.</p>
           </div>
         </div>
@@ -40,7 +41,7 @@ export default function Home() {
           <div className={styles.content}>
             <div className={styles.text}>
               <h1>Türkiye&apos;de ilk ve tek!</h1>
-              <p>Öğrencilerinizin tüm temel matematik eksiklerini hızla ve eksiksiz kapatın!</p>
+              <p><b>Öğrencilerinizin tüm temel matematik eksiklerini hızla ve eksiksiz kapatın!</b></p>
               <p>Türkiye&apos;nin 73 ilinde ve dünyada 7 ülkede, öğrenci ve velilerine %100&apos;e yakın memnuniyetle hizmet veren Matematik Röntgeni&apos;nin dijital ürünü olan Mathsey Matematik Robotu ile öğrencilerinizin tüm temel matematik eksiklerini hızla ve eksiksiz kapatın!</p>
             </div>
             <div className={styles.image}>
